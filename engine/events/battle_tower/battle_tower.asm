5922035898b0de867abd479ba29c831e68ebaf71
Ebernacher90 <danielvonderbank@gmail.com>
2021-02-06 22:20:03 +0100

    Fixed Battle Tower Typo

--- engine/events/battle_tower/battle_tower.asm
+++ engine/events/battle_tower/battle_tower.asm
@@ -578,7 +578,7 @@ CopyBTTrainer_FromBT_OT_TowBT_OTTemp:
 
 	ld a, BANK(sBattleTowerChallengeState)
 	call GetSRAMBank
-	ld a, BATTLETOWER_CHALLENGE_IN_PROGESS
+	ld a, BATTLETOWER_CHALLENGE_IN_PROGRESS
 	ld [sBattleTowerChallengeState], a
 	ld hl, sNrOfBeatenBattleTowerTrainers
 	inc [hl]
